5d8683445244063c7b7e44bd9e9350316cfdbcd5
Gian Lucca Sanza <gianluccalord723@gmail.com>
2024-04-02 22:46:30 -0300

    Termine el debugging del taller 2. Falta realizar la cobertura

--- taller2/src/main/java/aed/Debugging.java
+++ taller2/src/main/java/aed/Debugging.java
@@ -25,30 +25,32 @@ class Debugging {
 
     boolean ordenado(int[] xs) {
         boolean res = true;
-        for (int i = 0; i < xs.length; i++) {
+        for (int i = 0; i < xs.length - 1; i++) {
             if (xs[i] > xs [i+1]) {
                 res = false;
             }
+        
         }
         return res;
     }
 
     int maximo(int[] xs) {
-        int res = 0;
-        for (int i = 0; i <= xs.length; i++) {
-            if (xs[i] > res) res = i;
+        int res = xs[0];
+        for (int i = 0; i < xs.length; i++) {
+            if (xs[i] > res) {
+                res = xs[i];
+            }
         }
         return res;
     }
 
     boolean todosPositivos(int[] xs) {
-        boolean res = false;
+        boolean res = true;
         for (int x : xs) {
-            if (x > 0) {
-                res = true;
-            } else {
+            if (x <= 0) {
                 res = false;
             }
+
         }
         return res;
     }
